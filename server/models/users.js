6d09c65c569cf74fbb1f20c4bf379ff55fef4baf
Mark Reid <mark.francis.reid@gmail.com>
2015-01-23 15:55:16 -0800

    fleshes out user schema

--- server/models/users.js
+++ server/models/users.js
@@ -1,2 +1,11 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+
+usersSchema = new Schema({
+	name: String,
+	email: String
+})
+
+var User = mongoose.model('users', usersSchema)
+
+module.exports = User;
