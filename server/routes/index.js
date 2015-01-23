d8a9824a5ffa31d8c0a9d826ef524dedaec85868
Liorr Yehuda Morrison <liorr.morrison@gmail.com>
2015-01-23 12:08:37 -0800

    adds sample request to crunchbase API and adds request package from npm

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request')
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+
+  request('https://api.crunchbase.com/v/2/organization/crowdtilt?user_key=2c7e457b872b77f865562e75967f76ef', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+     res.send(body) // Print the google web page.
+   }
+  })
+
 });
 
 module.exports = router;
