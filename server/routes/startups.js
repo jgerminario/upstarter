27f6a661a704c8fc35554790e6e69a69e4594f69
Liorr Yehuda Morrison <liorr.morrison@gmail.com>
2015-01-23 16:44:33 -0800

    adds feature to save API calls in database and serve data as JSON

--- server/routes/startups.js
+++ server/routes/startups.js
@@ -6,21 +6,21 @@ var Startup = require('../models/startups')
 /* GET users listing. */
 router.get('/', function(req, res) {
  	request('https://api.crunchbase.com/v/2/organization/crowdtilt?user_key=2c7e457b872b77f865562e75967f76ef', function (error, response, body) {
-   		if (!error && response.statusCode == 200) {
-    		res.send(body) // Print the google web page.
-    		console.log(body)
-    		var newStartup = new Startup({
-    			body: body
-    		})
-    		newStartup.save(function(err){
-    			if (!err) {
-    				console.log('success')
-    			} else {
-    				console.log('fail')
-    			}
-    		})
-  		}
- 	})
+        if (!error && response.statusCode == 200) {
+            var name = JSON.parse(body).data.properties.name
+            var newStartup = new Startup({
+               name: name
+            })
+            newStartup.save(function(err){
+              if (!err) {
+                console.log('success')
+              } else {
+                console.log('fail')
+            }
+            res.send(newStartup)
+          })
+        }
+    })
 });
 
 module.exports = router;
