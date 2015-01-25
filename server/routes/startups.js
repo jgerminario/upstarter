32d03ad61f01427862ae51aef0cebe8a7a51719f
Mark Reid <mark.francis.reid@gmail.com>
2015-01-24 16:04:21 -0800

    calculates fundraiseRounds for one startup

--- server/routes/startups.js
+++ server/routes/startups.js
@@ -5,20 +5,30 @@ var Startup = require('../models/startups')
 
 /* GET users listing. */
 router.get('/', function(req, res) {
- 	request('https://api.crunchbase.com/v/2/organization/crowdtilt?user_key=2c7e457b872b77f865562e75967f76ef', function (error, response, body) {
+ 	request('https://api.crunchbase.com/v/2/organization/respond-com?user_key=2c7e457b872b77f865562e75967f76ef', function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var name = JSON.parse(body).data.properties.name
+            var fundraiseArray = JSON.parse(body).data.relationships.funding_rounds.items
+            var fundraiseRounds = []
+            var path = 'organization/' + JSON.parse(body).data.properties.permalink
+            console.log(path)
+            fundraiseArray.forEach(function(round){
+              fundraiseRounds.push({amount: round.name.match(/\d{4,}/)[0], date: round.created_at})
+            })
+            console.log(fundraiseRounds)
             var newStartup = new Startup({
-               name: name
+               name: name,
+               fundraiseRounds: fundraiseRounds,
+               slug: path
             })
             newStartup.save(function(err){
               if (!err) {
                 console.log('success')
               } else {
                 console.log('fail')
-            }
+              }
+            })
             res.send(newStartup)
-          })
         }
     })
 });
