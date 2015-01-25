4775fbe3cf29b0a970a38c9be655c1dc6fa9c7d4
jgerminario <jgerminario@gmail.com>
2015-01-25 00:51:30 -0800

    fixes broken tests (needed to have some response body returned or else timeout occurs) and implements mock api call for second route with mock json object)

--- server/routes/startups.js
+++ server/routes/startups.js
@@ -6,7 +6,6 @@ var Startup = require('../models/startups')
 /* GET users listing. */
 router.get('/', function(req, res) {
  	request('https://api.crunchbase.com/v/2/organization/robin?user_key=2c7e457b872b77f865562e75967f76ef', function (error, response, body) {
-      console.log(req);
         if (!error && response.statusCode == 200) {
             var name = JSON.parse(body).data.properties.name
             var path = 'organization/' + JSON.parse(body).data.properties.permalink
@@ -87,29 +86,29 @@ router.get('/', function(req, res) {
 
 
 router.get('/initial-seed', function(req, res) {
-  // request('https://api.crunchbase.com/v/2/organizations?organization_types=company&user_key=2c7e457b872b77f865562e75967f76ef&page=1&order=created_at+DESC', function (error, response, body) {
+  request('https://api.crunchbase.com/v/2/organizations?organization_types=company&user_key=2c7e457b872b77f865562e75967f76ef&page=1&order=created_at+DESC', function (error, response, body) {
+    // console.log(JSON.parse(body))
+      if (!error && response.statusCode == 200) {
+        var items = JSON.parse(body).data.items
+        var counter = 0
+        for (var i=0; i<items.length; i++) {
 
-  //     if (!error && response.statusCode == 200) {
-
-  //       var items = JSON.parse(body).data.items
-  //       var counter = 0
-  //       for (var i=0; i<items.length; i++) {
-
-  //         var newEntry = new Startup({
-  //           slug: items[i].path,
-  //           name: items[i].name
-  //         })
-  //         newEntry.save(function(err) {
-  //           if (!err) {
-  //             console.log(counter, 'success')
-  //             counter += 1
-  //           } else {
-  //             console.log('fail')
-  //           }
-  //         })
-  //       }
-  //     }
-  //  })
+          var newEntry = new Startup({
+            slug: items[i].path,
+            name: items[i].name
+          })
+          newEntry.save(function(err) {
+            if (!err) {
+              console.log(counter, 'success')
+              counter += 1
+            } else {
+              // console.log('fail')
+            }
+          });
+        }
+      res.send(JSON.parse(body));
+      }
+   })
 })
 
 module.exports = router;
--- server/test/controller_test.js
+++ server/test/controller_test.js
@@ -7,14 +7,22 @@ var express = require('express'),
   request = require('request'),
   nock = require('nock'),
   // expect = require('expect.js');
-  expect = require('chai').expect,
+  expect = require('chai').expect, // http://chaijs.com/api/bdd/
   startupTest = require('./test.json'),
-  api = nock("https://api.crunchbase.com")
+  startupsTest = require('./test_multi.json'),
+  single_api = nock("https://api.crunchbase.com")
       .filteringPath(function(path){
         return '/v/2/organization';
       })
+      // .persist() // if needed multiple times
       .get("/v/2/organization") //this needs to be the route - base url must be in the nock param
-      .reply(200, startupTest);
+      .reply(200, startupTest),
+  multi_api = nock("https://api.crunchbase.com")
+      .filteringPath(function(path){
+        return '/v/2/organizations';
+      })
+      .get("/v/2/organizations") //this needs to be the route - base url must be in the nock param
+      .reply(200, startupsTest);
 
 
 describe('server', function () {
@@ -30,26 +38,11 @@ describe('server', function () {
 
 
 
-  // describe('homepage', function(){
-  //   it('should respond to GET',function(done){
-  //     request
-  //       .get('http://localhost:' + port) 
-  //       .on('response', function(response){
-  //         expect(response.statusCode).to.equal(200);
-  //         done();
-  //       })
-  //       .on('error', function(err){
-  //         console.log(err);
-  //       });
-  //     });
-  //   });
-
-  describe('startup endpoint', function(){
+  describe('homepage', function(){
     it('should respond to GET',function(done){
       request
-        .get('http://localhost:' + port + '/startups') 
+        .get('http://localhost:' + port) 
         .on('response', function(response){
-          expect(response).to.be.an('object');
           expect(response.statusCode).to.equal(200);
           done();
         })
@@ -61,11 +54,29 @@ describe('server', function () {
 
   describe('startup endpoint', function(){
     it('should respond to GET',function(done){
-      console.log('test');
+      request
+        .get('http://localhost:' + port + '/startups') // function(error, response, body) is alternative, this seems more robust but also harder to get data out
+        .on('data', function(data){
+          console.log(data.toString()); // shows what data comes in asynchronously
+        })
+        .on('response', function(response){
+          expect(response.statusCode).to.equal(200); // response has no body in this case, need to gather from data above to get full body
+          done();
+        })
+        .on('error', function(err){
+          console.log(err);
+        });
+      // assert.ok(true);
+      // assert.equal(result, 'bar');
+      });
+    });
+
+  describe('startup endpoint', function(){
+    it('should respond to GET',function(done){
       request
         .get('http://localhost:' + port + '/startups/initial-seed') 
         .on('response', function(response){
-          expect(response.status).to.equal(200);
+          expect(response.statusCode).to.equal(200);
           done();
         })
         .on('error', function(err){
