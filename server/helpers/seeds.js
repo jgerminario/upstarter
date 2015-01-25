2c108349a50dc0677892a358456d33f220490cbb
jgerminario <jgerminario@gmail.com>
2015-01-25 02:16:14 -0800

    adds helper methods for identifying random startup for api call

--- server/helpers/seeds.js
+++ server/helpers/seeds.js
@@ -4,47 +4,37 @@ var router = express.Router();
 var request = require('request')
 var Startup = require('../models/startups');
 
+var startupAPI = (function (){
 
-var getAllStartups = function (){
-  Startup.find(function(err, startups){
-    var count = startups.length;
-    // console.log(count);
-    // var rand_num = Math.floor(Math.random() * count);
-    // var startup_to_update = startups[371];
-    // console.log(startup_to_update);
-    Startup.findById("54c3fe2f98a0ee617841ef35", function(err, startup_to_update){
-      console.log(startup_to_update);
+  var findRandomStartup = function (err, startups){
+    var count = startups.length,
+        rand_num = Math.floor(Math.random() * count),
+        startup_to_update = startups[rand_num];
     while (startup_to_update.description){
-      console.log("Test");
-      var rand_num = Math.floor(Math.random() * count);
+      rand_num = Math.floor(Math.random() * count);
       startup_to_update = startups[rand_num];
     }
-      console.log(rand_num);
-      console.log(startup_to_update);
-      Startup.findById(startup_to_update.id, function(err, startup){
-        startup.description = "test";
-        startup.save(function(err, data){
-          console.log(err);
-          console.log(data);
-          console.log(startup);
-        startup.description = undefined;
-        console.log(startup);
-        });
-      });
-      // console.log(startup_to_update.id)
-      // Startup.update({ _id: startup_to_update.id}, { $set: { description: "test"}}, function(err, data){
-      // });
-      //   console.log(data);
-      //   console.log(startup_to_update);
-      //   Startup.update({ _id: startup_to_update.id}, { $unset: { description: 1}}, function(err, data){
-      //     console.log(data);
-      //     console.log(startup_to_update);
-      //   Startup.update({});
-      //   });
-    // });
-  // });
+    Startup.findById(startup_to_update.id, makeAPICall);
+  };
 
-});
-});
-};
-module.exports = getAllStartups;
+  var makeAPICall = function (err, startup){
+
+  };
+
+  return {
+    updateStartup: function(){
+      Startup.find(findRandomStartup);
+    }
+  };
+
+})();
+
+// var getAllStartups = function (){
+    // console.log(count);
+    // var startup_to_update = startups[371];
+    // console.log(startup_to_update);
+    // Startup.findById("54c3fe2f98a0ee617841ef35", function(err, startup_to_update){
+    // });
+// };
+
+module.exports = startupAPI;
