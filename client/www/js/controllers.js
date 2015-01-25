094ea7565759bc8cb2ba2561fac3d029e92c9b3a
Joe Zuiches <jzuiches@yahoo.com>
2015-01-24 18:45:28 -0800

    adds a select area for search

--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -10,17 +10,13 @@ angular.module('upstarter.controllers', [])
     // $http.get("http://api.crunchbase.com/v/2/organization/crowdtilt?user_key=2c7e457b872b77f865562e75967f76ef").success(function(data){
   }])
 
-.controller('SearchCtrl', ['$scope', 'StartupNames', 'Test', function($scope, StartupNames, Test) {
-    $scope.test = "This is a test"
+.controller('SearchCtrl', ['$scope', 'StartupNames', function($scope, StartupNames) {
+
     StartupNames.then(function(data){
       $scope.startups = data;
+      console.log(data)
     })
-    console.log(Test)
+
     // $http.get("http://api.crunchbase.com/v/2/organization/crowdtilt?user_key=2c7e457b872b77f865562e75967f76ef").success(function(data){
   }])
 
-.controller('CompanyCtrl', ['$scope', 'StartupNames', 'Companies', function($scope, StartupNames) {
-        StartupNames.then(function(data){
-            $scope.startups = data;
-        })
-  }])
