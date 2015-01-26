e39d8a116743414df8b3374c097eaa099a123c40
jgerminario <jgerminario@gmail.com>
2015-01-26 14:26:49 -0800

    fixes issues with fundraise percentile and calculates only for those with momentum score

--- server/models/startups.js
+++ server/models/startups.js
@@ -48,34 +48,44 @@ startupsSchema = new Schema({
 //       totalRate = total/years
 //       return totalRate;
 // };
-
+startupsSchema.statics.resetFundraisePercentile = function () {
+    this.find().exec(function(err, data){
+      data.forEach(function(company){
+        company.fundraisePercentile = 0;
+        company.save(function(err, data){
+          if (err) { console.log(err); }
+          console.log(company.name + " " + company.fundraisePercentile)
+        });
+      });
+    });
+}
 
 startupsSchema.statics.calculateFundraisePercentile = function () {
   // console.log(Startup.find().exec(function(err,docs){
   //   console.log(docs);
   // }));
-  console.log("test")
-  var self = this
 
-  this.where({ 'acquired': false, 'public': false, 'closed': false, momentumScore: {$gt: 0} }).count().exec(function(err,count){
-    console.log("test3")
-    self.find().sort([['momentumScore', 'ascending']]).exec(function(err, docs){
-      console.log("test2")
+  // this.where({ 'acquired': false, 'public': false, 'closed': false, momentumScore: {$gt: 0} }).count().exec(function(err,count){
+    // console.log("test3")
+    this.find().where({ 'acquired': false, 'public': false, 'closed': false, momentumScore: {$gt: 0} }).sort([['momentumScore', 'ascending']]).exec(function(err, docs){
+      // console.log("test2")
+      var count = docs.length
       // console.log(docs);
       // console.log(count);
       docs.forEach(function(company, index){
-        if (company.closed || company.acquired || company.public || company.momentumScore === 0) {
-          company.fundraisePercentile = 0;
-        } else {
+        // if (company.closed || company.acquired || company.public || company.momentumScore == 0 || company.momentumScore == null) {
+        //   console.log(company.fundraisePercentile)
+        //   company.fundraisePercentile = 0;
+        // } else {
           company.fundraisePercentile = (index+1)/(count+1) * 100;
-        }
-        console.log(index + " " + company + " " + company.fundraisePercentile)
-        company.save(function(err){
+        // }
+        console.log(index + " " + company.name + " " + company.momentumScore + " " + company.fundraisePercentile)
+        company.save(function(err, data){
           if (err) {
             return "error saving " + company.name
           }
+          // console.log(data);
         });
-      });
       // R/(c+1)*100
     });
   });
