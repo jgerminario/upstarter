5218eb561303aa44b5ae482d259cadf52ad37ebd
jgerminario <jgerminario@gmail.com>
2015-01-26 13:34:25 -0800

    adds method for reseting fundraise percentile

---
+++ server/helpers/reset_fundraise_percentile.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var Startup = require('../models/startups');
+var organizationEndpoint = require('./api.js');
+var startupAPI = require('./seeds');
+
+mongoose.connect('mongodb://admin:upstarter@ds041157.mongolab.com:41157/upstarter');
+
+Startup.calculateFundraisePercentile();
