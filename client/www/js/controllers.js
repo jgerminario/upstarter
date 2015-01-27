be51c042dd3b2cb81ddce43f1c85577f0d639b51
Joe Zuiches <jzuiches@yahoo.com>
2015-01-26 16:54:31 -0800

    updates slider without .value being passed to SearchCtrl

--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -10,8 +10,10 @@ angular.module('upstarter.controllers', [])
     // $http.get("http://api.crunchbase.com/v/2/organization/crowdtilt?user_key=2c7e457b872b77f865562e75967f76ef").success(function(data){
   }])
 
-.controller('SearchCtrl', ['$scope', 'StartupNames', function($scope, StartupNames) {
+.controller('SearchCtrl', ['$scope', 'StartupNames', 'EmployeeRange', function($scope, StartupNames, EmployeeRange) {
 
+
+    $scope.value = EmployeeRange.data;
     StartupNames.then(function(data){
       $scope.startups = data;
       console.log(data)
@@ -21,8 +23,10 @@ angular.module('upstarter.controllers', [])
     // $http.get("http://api.crunchbase.com/v/2/organization/crowdtilt?user_key=2c7e457b872b77f865562e75967f76ef").success(function(data){
   }])
 
-.controller('SliderCtrl', ['$scope', function($scope){
-  $scope.value = "0;10000";
+.controller('SliderCtrl', ['$scope', 'EmployeeRange',function($scope, EmployeeRange){
+
+  $scope.value = EmployeeRange.data;
+
   $scope.options = {
     from: 0,
     to: 10000,
@@ -37,4 +41,5 @@ angular.module('upstarter.controllers', [])
           pointer: {"background-color": "red"}
         }
   };
+
 }]);
